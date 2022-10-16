43299d11aae54ee2bd50b09838fc43791ca1b6e2
subaru-hello <octosubaru0926@gmail.com>
2022-10-16 17:29:37 +0900

    Object.assign

--- javascripts/maniplate_object/index.js
+++ javascripts/maniplate_object/index.js
@@ -1,10 +1,60 @@
-const original = { a: 1, b: 2, c: 3 };
+const response = {
+  data: [
+    {
+      name: "Wiskey",
+      price: "350",
+      percentage: "10",
+    },
+    {
+      name: "Lemon Sour",
+      price: "350",
+      percentage: "5",
+    },
+    {
+      name: "Beer",
+      price: "350",
+      percentage: "5",
+    },
+    {
+      name: "Plum shu",
+      price: "350",
+      percentage: "12",
+    },
+    {
+      name: "Vodka",
+      price: "350",
+      percentage: "24",
+    },
+  ],
+};
 
-//copy
-const copy = Object.assign({}, original);
-console.log(copy);
-console.log(copy === original);
+const { data: alcohols = [] } = response;
 
-const assigned = Object.assign(original, { c: 10, d: 50 }, { d: 100 });
-console.log(assigned);
-console.log(original);
+const copy_alcohol = Object.assign({}, alcohols);
+console.log(copy_alcohol === alcohols); // false
+
+const assign_attributes = {
+  data: [
+    {
+      name: "highball",
+      price: "200",
+      percentage: "5",
+    },
+    {
+      name: "sake",
+      price: "1000",
+      percentage: "23",
+    },
+  ],
+};
+
+const { data: update = []} = assign_attributes;
+
+const assigned_alcohols = Object.assign(copy_alcohol, update);
+// {
+//     '0': { name: 'highball', price: '200', percentage: '5' },
+//     '1': { name: 'sake', price: '1000', percentage: '23' },
+//     '2': { name: 'Beer', price: '350', percentage: '5' },
+//     '3': { name: 'Plum shu', price: '350', percentage: '12' },
+//     '4': { name: 'Vodka', price: '350', percentage: '24' }
+//   }
